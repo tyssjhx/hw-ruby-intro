7524fb6b746c84d73739e85229d1ec38b0685a91
root <wangjingpu17@mails.ucas.ac.cn>
2021-10-19 10:52:59 +0800

    add: Task1&2 Complete

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,33 +3,92 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  sum = 0;
+  if arr.size == 0
+    return 0;
+  end
+  for i in 0...arr.size
+    sum += arr[i];
+  end
+  return sum;
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  size = arr.size
+  if size == 0
+    return 0;
+  elsif size == 1;
+    return arr[0];
+  end
+  sum = 0;
+  max1 = -1000;
+  max2 = -1000;
+  for i in 0...size
+    if arr[i] > max1
+      max1 = arr[i];
+    elsif arr[i] > max2
+      max2 = arr[i]
+    end
+  end
+  return max1+max2
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  size = arr.size
+  if size == 0
+    return false;
+  elsif size == 1
+    return false;
+  end
+  for i in 0...size
+    for j in (i+1)...size
+      if arr[i] + arr[j] == n
+        return true
+      end
+    end
+  end
+  return false
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  return "Hello, " + name
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  if s.size == 0
+    return false
+  end 
+  c = s[0].capitalize
+  if (c >= 'A' && c <= 'Z') 
+    if (c == 'A') || (c == 'E') || (c == 'I') || (c == 'O') || (c == 'U')
+      return false
+    else
+      return true
+    end
+  else
+    return false
+  end
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  size = s.size
+  if size == 1
+    return s[0] == "0"? true : false
+  end
+  for i in 0...size
+    if (s[i] != "0") && (s[i] != "1")
+      return false;
+    end
+  end
+  if (s[-1] == "0" && s[-2] == "0")
+    return true;
+  end
+  return false;
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
 end
